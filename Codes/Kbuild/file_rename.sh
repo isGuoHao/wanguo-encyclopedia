03a9ca2a81d2803e5cde9a78cd12eeae32f09229
isGuoHao <guohaoprc@163.com>
2024-11-17 13:03:40 +0800

    Issues: Rename bsp to pdc, osal to osa Date: 11/17/24 13:03:40 Author: wanguo

---
+++ Codes/Kbuild/file_rename.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# 定义要替换的关键字
+old_keyword="bsp"
+new_keyword="pdc"
+
+# 使用find命令查找当前目录下所有包含旧关键字的文件和文件夹
+# -depth选项确保先处理子文件夹中的项目
+find . -depth -name "*$old_keyword*" | while read file; do
+    # 计算新文件名
+    new_file=$(echo "$file" | sed "s/$old_keyword/$new_keyword/g")
+    
+    # 检查新旧文件名是否相同，如果不同则重命名
+    if [ "$file" != "$new_file" ]; then
+        echo "Renaming '$file' to '$new_file'"
+        mv "$file" "$new_file"
+    fi
+done
